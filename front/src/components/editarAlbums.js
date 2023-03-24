d9c76a7f54955223f2248965e05115b2094ce30a
Magdiel Asicona <3275257541405@ingenieria.usac.edu.gt>
2023-03-24 16:49:16 -0600

    Extraer texto

--- front/src/components/editarAlbums.js
+++ front/src/components/editarAlbums.js
@@ -8,32 +8,96 @@ import axios from "axios";
 import FileInput from "./FileInput";
 
 function AlbumsGrid(props) {
-    
+    const [base64Image, setBase64Image] = useState('');
+    const [ImgUrl, setImgUrl] = useState("");
+    const [texto, setTexto] = useState("");
+    const [userData, setUserData] = useState({
+        image: []
+    })
+
+    const handleInputChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleFilesChange = (event) => {
+        const files = event.target.files;
+        if(!event.target.files[0]){
+            return;
+        }
+        
+        if (userData.image.length > 0) {
+            userData.image[0] = (event.target.files[0]);
+        } else {
+            userData.image.push(event.target.files[0]);
+        }
+        setImgUrl(URL.createObjectURL(event.target.files[0]));
+        const file =  files[0]
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          setBase64Image(reader.result.split(',')[1]);
+        };
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const objectImage = {
+            imagen: base64Image
+        }
+       
+        axios.post('http://localhost:5000/rekognition/detectartexto', objectImage)
+            .then(res => {
+                let textos = "";
+                console.log(res.data);
+                res.data.texto.forEach(element => {
+                    //console.log(element.DetectedText)
+                    textos += element.DetectedText + " ";
+                });
+                setTexto(textos);
+                textos = ""
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
 
 
     return (
         <div className="Dashboard-Grid">
             
             <Grid>
-            <Grid.Column width={8} >
-                <Grid.Row>
-                    <FileInput   title={"Foto"}></FileInput>
+            <Grid.Column width={5} >
+                <Grid.Row >
+                    <FileInput change={handleFilesChange} ImgUrl={ImgUrl}  title={"Foto"}></FileInput>
                 </Grid.Row>
-            </Grid.Column >
-            <Grid.Row >
+                <Grid.Row >
+                
                 <div className="Register-Form-Button">
-                    <Button className="ui black button" type="submit" >Extraer texto</Button>
+                    <Button className="ui black button" type="submit" onClick={handleSubmit} >Extraer texto</Button>
                 </div>
             </Grid.Row>
-            <Grid.Row centered >
+            </Grid.Column >
+
+            
+           
+            
+            
+            <Grid.Column width={5} >
+            <Grid.Row  >
                 <div className="form" style={{ textAlign: "center" }}>
                     <TextArea
-                        style={{ minHeight: 200 }}
-                        Column={16}
-                        Row={16}
+                        style={{ minHeight: 300, minWidth: 500 }}
+                        value={texto}
                     />
                 </div>
-            </Grid.Row>        
+            </Grid.Row> 
+            </Grid.Column>
+                   
 
             </Grid>
         </div>
